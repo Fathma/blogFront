9a0e0912041f49430c3bcd9ee4fe1a4ab00dec91
Fathma <siddiquefathma@gmail.com>
2019-10-22 17:30:34 +0600

    get jwt

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {Component} from 'react';
 import List from './components/child/list'
 import Reg from './components/child/register'
+import Login from './components/child/login'
+
+import{ BrowserRouter, Switch, Route} from 'react-router-dom'
 
 import './App.css';
 
@@ -9,7 +12,14 @@ class App extends Component {
   
   render(){
     return (
-      <Reg />
+      <BrowserRouter >
+        <Switch>
+          <Route path="/list" exact component={List} />
+          <Route path="/register" exact component={Reg} />
+          <Route path="/login" exact component={Login} />
+
+        </Switch>
+      </BrowserRouter>
     );
   }
  
---
+++ src/components/child/login.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import {Component} from 'react'
+import axios from 'axios'
+
+import '../../App.css';
+
+class Reg extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+        this.change = this.change.bind(this)
+        this.submit = this.submit.bind(this)
+    }
+    change(e){
+        this.setState({
+            [e.target.name] : e.target.value
+
+        })
+    }
+
+    submit(e){
+        e.preventDefault()
+        axios.post('/user/login', { email: this.state.email, password: this.state.password }).then(res => {
+            localStorage.setItem('jwt', res.data)})
+    }
+ 
+  render(){
+    return (
+        <div className="App">
+            <form onSubmit={ e => this.submit(e)}>
+                <input type="text" name="email" onChange = { e => this.change(e) } value={this.state.email} />
+                <input type="password"  name="password" onChange = { e => this.change(e) } value={this.state.password}/>
+                <button type="submit">Submit</button>
+            </form>
+            <a href="http://localhost:5000/user/google" >Login using Google.</a> <br></br>
+            <a href="http://localhost:5000/user/facebook" >Login using Facebook</a>
+        </div>
+    );
+  }
+ 
+}
+
+export default Reg;
